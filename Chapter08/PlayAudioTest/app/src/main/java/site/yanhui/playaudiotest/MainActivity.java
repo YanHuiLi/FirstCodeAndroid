1c4cbec9526140d771c86c2d6ac3b811463b1098
Archer <421228286@qq.com>
2017-09-01 09:05:08 +0800

    complete the PlayAudioTest

--- Chapter08/PlayAudioTest/app/src/main/java/site/yanhui/playaudiotest/MainActivity.java
+++ Chapter08/PlayAudioTest/app/src/main/java/site/yanhui/playaudiotest/MainActivity.java
@@ -1,13 +1,112 @@
 package site.yanhui.playaudiotest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //创建一个音乐播放器
+    private MediaPlayer mediaPlayer = new MediaPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initUI();
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            //申请权限
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        } else {
+            initMediaPlayer();
+        }
+
+    }
+
+    //初始化UI 的方法
+    private void initUI() {
+        Button Play = (Button) findViewById(R.id.Play);
+        Play.setOnClickListener(this);
+        Button Pause = (Button) findViewById(R.id.Pause);
+        Pause.setOnClickListener(this);
+        Button Stop = (Button) findViewById(R.id.Stop);
+        Stop.setOnClickListener(this);
+    }
+
+    //初始化播放器
+    private void initMediaPlayer() {
+        //这里拿到的getExternalStorageDirectory在我的手机上 并不是sd卡，而是本地存储的根目录
+        File file = new File(Environment.getExternalStorageDirectory(),"Mp3.mp3");
+        try {
+            mediaPlayer.setDataSource(file.getPath());
+            mediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //权限的回调函数
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    initMediaPlayer();
+                } else {
+                    Toast.makeText(this, "拒绝权限无法使用程序", Toast.LENGTH_SHORT).show();
+                    finish();//结束程序
+                }
+        }
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.Play:
+                Toast.makeText(this, "点击Play", Toast.LENGTH_SHORT).show();
+                if (!mediaPlayer.isPlaying()) {
+                    mediaPlayer.start();//开始播放
+                }
+                break;
+            case R.id.Pause:
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();//暂停播放
+                }
+                break;
+
+            case R.id.Stop:
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.reset(); //停止播放
+                    initMediaPlayer();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer!=null){
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
     }
 }
