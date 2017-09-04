449793c64730087b96f1375b7863f4d5cc5a3a88
Archer <421228286@qq.com>
2017-09-04 09:26:21 +0800

    complete the Service's study

--- Chapter10/ServiceTest/app/src/main/java/site/yanhui/servicetest/MainActivity.java
+++ Chapter10/ServiceTest/app/src/main/java/site/yanhui/servicetest/MainActivity.java
@@ -1,11 +1,39 @@
 package site.yanhui.servicetest;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import site.yanhui.servicetest.service.MyIntentService;
+import site.yanhui.servicetest.service.MyService;
+
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private static final String TAG = "MainActivity";
+    //拿到DownBinder的实例开启下载功能
+    private MyService.DownLoadBinder downLoadBiner;
+    //用serviceConnection和服务通讯
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            downLoadBiner = (MyService.DownLoadBinder) service;
+            downLoadBiner.startBinder();
+            downLoadBiner.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,23 +42,61 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         initUI();
     }
 
+    /**
+     * 1.初始化组件
+     * 2。绑定监听
+     */
     private void initUI() {
         Button StartService = (Button) findViewById(R.id.start_service);
         Button StopService = (Button) findViewById(R.id.stop_service);
         StartService.setOnClickListener(this);
         StopService.setOnClickListener(this);
+
+        Button bindService = (Button) findViewById(R.id.Bind_service);
+        Button UnbindService = (Button) findViewById(R.id.unbind_service);
+        bindService.setOnClickListener(this);
+        UnbindService.setOnClickListener(this);
+
+        Button StartIntentService= (Button) findViewById(R.id.start_intent_service);
+         StartIntentService.setOnClickListener(this);
     }
 
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.start_service:
-
+                Intent StartServiceIntent = new Intent(this, MyService.class);
+                startService(StartServiceIntent);
                 break;
 
             case R.id.stop_service:
+                Intent StopServiceIntent = new Intent(this, MyService.class);
+                stopService(StopServiceIntent);
+                break;
+
+            case R.id.Bind_service:
+
+                Intent bindIntent = new Intent(this, MyService.class);
+                bindService(bindIntent,serviceConnection,BIND_AUTO_CREATE);//绑定服务只有onCreate方法执行
 
                 break;
+            case R.id.unbind_service:
+                unbindService(serviceConnection);//解绑服务
+                break;
+
+            case R.id.start_intent_service:
+                Log.d(TAG, "the Main process Id is "+ Thread.currentThread().getId());
+                Intent intent = new Intent(this, MyIntentService.class);
+                startService(intent);
+                break;
+            default:
+                break;
+
         }
     }
 }
---
+++ Chapter10/ServiceTest/app/src/main/java/site/yanhui/servicetest/service/MyService.java
@@ -0,0 +1,93 @@
+package site.yanhui.servicetest.service;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+import site.yanhui.servicetest.MainActivity;
+import site.yanhui.servicetest.R;
+
+/**
+*create at 2017/9/3 by 10:59
+*@author Archer
+*@function
+*/
+
+public class MyService extends Service {
+
+
+    private static final String TAG = "MyService";
+
+   //构造
+    public MyService() {
+    }
+
+
+
+
+    private   DownLoadBinder mBinder= new DownLoadBinder();
+    /**
+     * 模拟一个类来完成下载的功能
+     */
+   public  class DownLoadBinder extends Binder {
+
+        //开始下载
+        public void startBinder() {
+            Log.d(TAG, "startBinder: executed");
+        }
+
+        //得到进度
+        public int getProgress() {
+            Log.d(TAG, "getProgress: executed");
+            return 0;
+        }
+
+    }
+
+    /**
+     *
+     * @param intent 传入一个intent对象
+     * @return  返回一个IBinder类型的数据
+     */
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate: ");
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent= PendingIntent.getActivity(this,0,intent,0);
+        Notification notification=new  android.support.v4.app.NotificationCompat.Builder(this)
+                 .setContentText("这是一个前台服务")
+                .setContentTitle("前台服务")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .build();
+         startForeground(1,notification);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand: "); //放在return之前
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+}
