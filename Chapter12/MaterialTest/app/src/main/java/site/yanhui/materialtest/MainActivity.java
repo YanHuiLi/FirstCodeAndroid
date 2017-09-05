31e756c2530dcc42ce825e78e12b8871fcbc7063
Archer <421228286@qq.com>
2017-09-05 14:44:53 +0800

    完成drawerLayout

--- Chapter12/MaterialTest/app/src/main/java/site/yanhui/materialtest/MainActivity.java
+++ Chapter12/MaterialTest/app/src/main/java/site/yanhui/materialtest/MainActivity.java
@@ -1,7 +1,10 @@
 package site.yanhui.materialtest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -9,34 +12,47 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar= (Toolbar) findViewById(R.id.toolBar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolBar);
         setSupportActionBar(toolbar);
+        //滑动菜单的使用抽屉菜单
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBar actionBar = getSupportActionBar();//获得actionbar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);//设置指示器 图片不是很适配
+
+        }
+
     }
 
     /**
-     *
      * @param menu 传入的菜单
      * @return 返回true则成功解析。false则不成功
      */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.toolbar,menu);
+        getMenuInflater().inflate(R.menu.toolbar, menu);
         return true;
     }
 
     /**
      * menu 添加的条目
      *
-     * @param item  条目
-     * @return  返回true就创建，false就不创建
+     * @param item 条目
+     * @return 返回true就创建，false就不创建
      */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);//点击打开抽屉
+                break;
             case R.id.backUp:
                 Toast.makeText(this, "you click backup", Toast.LENGTH_SHORT).show();
                 break;
@@ -45,8 +61,8 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.setting:
                 Toast.makeText(this, "you click  setting", Toast.LENGTH_SHORT).show();
-                default:
-                    break;
+            default:
+                break;
         }
         return true;
     }
